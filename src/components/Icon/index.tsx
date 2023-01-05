a900eb3c104eaed41978c7ca89953c716dedd7a5
Vladislav <lipatov.xex@gmail.com>
2023-01-05 17:49:44 +0700

    Added comments

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,5 +1,4 @@
 import type { VoidComponent } from "solid-js";
-import { mergeProps } from "solid-js";
 import clsx from "clsx";
 
 // This can be imported globaly for caching
@@ -7,18 +6,15 @@ import iconsSvg from "./assets/icons.svg";
 
 import styles from "./Icon.module.scss";
 
-import "./assets/icons.svg";
-
 export type IconName = "info" | "arrow-right" | "spinner";
 
 interface Props {
   name: IconName;
-
   class?: string;
 }
 
 export const Icon: VoidComponent<Props> = (props) => (
   <svg class={clsx(props.class, styles.icon)}>
-    <use href={`${iconsSvg}#${props.name}`}></use>
+    <use href={`${iconsSvg}#${props.name}`} />
   </svg>
 );
